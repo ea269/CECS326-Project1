657c806dab404d4f5471c6f9c678ef14ee632d60
Rhino004 <ryantomas85@gmail.com>
2024-09-18 16:17:23 -0700

    adding a check if there is 2 paremeter

--- filecopy.c
+++ filecopy.c
@@ -16,6 +16,14 @@
 
 int main(int argc, char *argv[])
 {
+	if (argc != 3) //Checks if there more or less than 2 parameter
+	{
+		cout <<"Please enter 2 parameters: " << endl;
+		cout <<"	Parameter 1: source file"<< endl;
+		cout <<"	parameter 2: destination file"<< endl;
+		exit(EXIT_SUCCESS);
+	}
+	return 0;
 	/*
 	Your code is here
 	*/
