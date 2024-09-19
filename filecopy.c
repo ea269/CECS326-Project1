6a4079bd980c9c5a0daf05ec062b224efb90053f
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-18 22:56:48 -0700

    touched up argument checker print statements

--- filecopy.c
+++ filecopy.c
@@ -19,9 +19,10 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3) //Checks if there more or less than 2 parameter
 	{
-		printf("Please enter 2 parameters: ");
-		printf("	Parameter 1: source file");
-		printf("	parameter 2: destination file");
+		printf("Please enter 2 parameters:\n");
+		printf("Parameter 1: Source file.\n");
+		printf("Parameter 2: Destination file.\n");
+        printf("Example: filecopy source.txt destination.txt\n");
 		exit(EXIT_SUCCESS);
 	}
 	return 0;
