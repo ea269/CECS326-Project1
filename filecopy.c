a5b92628b5713784112693ada2f2011d654297b9
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-19 09:43:00 -0700

    Some understanding how forking works

--- filecopy.c
+++ filecopy.c
@@ -16,6 +16,7 @@
 #define WRITE_END 1
 
 int main(int argc, char *argv[]) {
+    /*
 	if (argc != 3) // Checks if there more or less than 2 parameter {
 		printf("Please enter 2 parameters:\n");
 		printf("Parameter 1: Source file.\n");
@@ -23,6 +24,27 @@ int main(int argc, char *argv[]) {
         printf("Example: filecopy source.txt destination.txt\n");
 		exit(EXIT_SUCCESS);
 	}
+    */
+
+    printf("Forking will happen soon..\n");
+    
+    int id = fork();
+    // clone the calling process, creating an exact copy
+    // fork returns -1 for errors 0 for new processes (child)
+    printf("Fork id is %d\n", id);
+    
+    // each fork () is 2^n execution from there
+    if (id != 0) { // will only fork if its in main process (how we get odd number of forks)
+        fork();
+    }
+
+    if (id == 0) { // 0 = chilid
+        printf("Child Process\n");
+    } else { // a big random number = main
+        printf("Main Process\n");
+    }
+
+    printf("Hello world\n");
 
 	return 0;
 }
