93140109182ed9aa55d8b833c023794b7f6797b5
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-24 21:19:30 -0700

    Defined buffer size, cleaned up code, debugging piping

--- filecopy.c
+++ filecopy.c
@@ -14,50 +14,51 @@
 
 #define READ_END 0
 #define WRITE_END 1
+#define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {// Checks if there more or less than 2 parameter
-		printf("Please enter 2 parameters:\n");
-		printf("Parameter 1: Source file.\n");
-		printf("Parameter 2: Destination file.\n");
+    if (argc != 3) {  // Checks if there more or less than 2 parameter
+        printf("Please enter 2 parameters:\n");
+        printf("Parameter 1: Source file.\n");
+        printf("Parameter 2: Destination file.\n");
         printf("Example: filecopy source.txt destination.txt\n");
-		exit(EXIT_SUCCESS);
-	}
-    
+        exit(EXIT_SUCCESS);
+    }
+
     // Read/Write file
     FILE *f1 = fopen(argv[1], "r");
     FILE *f2 = fopen(argv[2], "w");
 
     // Piping
-    int fd[2]; // f[0] - read, f[1] - write
-    if (pipe(fd) == -1) { // -1 returning from pipe indicates error
+    int fd[2];  // f[0] - read, f[1] - write
+    if (pipe(fd) == -1) {  // -1 returning from pipe indicates error
         printf("An error occured with opening the pipe\n");
         exit(EXIT_FAILURE);
     }
 
     // Forking
-    int id = fork(); // returns 0 for child process, a big num for parent process
-    if (id == -1) { // -1 returning from fork indicates error
+    int id = fork();  // returns 0 for child, big num for parent process
+    if (id == -1) {  // -1 returning from fork indicates error
         printf("An error occured with forking\n");
         exit(EXIT_FAILURE);
     }
 
-    if (id == 0) { // child process 
+    if (id == 0) {  // child process
         close(fd[READ_END]);
-        if (write(fd[WRITE_END], f2, EOF) == -1) { // -1: error, 0 end of file, returns number written
+        if (fwrite(fd[WRITE_END], f2, EOF) == -1) { // -1: error, 0 end of file, returns number written
             printf("An error occured with child writing to the pipe\n");
             exit(EXIT_FAILURE);
         }
         close(fd[WRITE_END]);
 
-    } else { // parent process
+    } else {  // parent process
         close(fd[WRITE_END]);
-        if (read(fd[READ_END], f1, EOF) == -1) { // -1: error, 0 end of file, returns number read
+        if (fread(fd[READ_END], f1, size) == -1) {  // -1: error, 0 end of file, returns number read
             printf("An error occured with parent writing to the pipe\n");
             exit(EXIT_FAILURE);
         }
         close(fd[READ_END]);
     }
 
-	return 0;
+    return 0;
 }
